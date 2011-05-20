a8a8422606d9d055206957815da975682e3b5ecf
Matt Post <post@jhu.edu>
2011-05-20 16:52:05 -0400

    updated test case

--- test/test-signature.pl
+++ test/test-signature.pl
@@ -5,5 +5,5 @@ use warnings;
 use CachePipe;
 
 print "The following two lines should be the same\n";
-print "144894520b22ca1bf2881d51a5bb847b39fc3cc0\n";
+print "a8a940627d132695a9769df883f85992f0ff4a43\n";
 print signature("this is a test") . $/;
